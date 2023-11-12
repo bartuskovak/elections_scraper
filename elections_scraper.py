2cd43106b4e1e5c61235a9d175e2338ab1a3c702
bartuskovak <kati.bartuskova@gmail.com>
2023-11-12 16:56:34 +0100

    final version

--- elections_scraper.py
+++ elections_scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import unicodedata
 import pandas as pd
 
+
 def read_input_params(argv):
     """
     This function reads input arguments. First argument: url, second argument: output file name.
@@ -29,38 +30,35 @@ def process_table(root_url, table):
     link_cols = table.find_all("td", "cislo")
     for link_col in link_cols:
         a = link_col.find("a")
-        link = {"url": root_url + a["href"], "code": a.string}
+        link = {"url": root_url + "/" + a["href"], "code": a.string}
         links.append(link)
     return links
 
 
 def get_districts(page_url):
     """
-    This function gets root url from input url.
-    :param page_url:
-    :return:
+    This function gets root url from input url and finds all tables on the page and processes them.
+    :param page_url: program input url
+    :return: list of districts
     """
-    # get root url from input url
-    root_url = "https://www.volby.cz/pls/ps2017nss/"
+    url_parts = page_url.rsplit("/", 1)
+    root_url = url_parts[0]
 
-    # get main page
     response = get(page_url)
     page = BeautifulSoup(response.text, features="html.parser")
 
-    # find all tables on the page and process them
     tables = page.find_all("table", "table")
     out_districts = []
     for tbl in tables:
         out_districts.extend(process_table(root_url, tbl))
 
-    return page, out_districts
+    return out_districts
 
-def create_output(main_page, districts):
+
+def create_output(districts):
     """
-    This function gets all data that we need and creates output
-    :param main_page:
-    :param districts:
-    :return:
+    This function gets all needed data from district tables and creates output csv file
+    :param districts: list of district urls and ids
     """
     out_list = []
     for district in districts:
@@ -96,10 +94,10 @@ def create_output(main_page, districts):
     df = pd.DataFrame(out_list)
     df.to_csv(out_file_name, index=False, header=True)
 
-# main entry point
+
 if __name__ == '__main__':
 
-    url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2109"
+    #url = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2109"
     out_file_name = "output.csv"
 
     try:
@@ -107,8 +105,6 @@ if __name__ == '__main__':
     except Exception as err:
         print(err)
 
-    main_page, districts = get_districts(url)
+    districts = get_districts(url)
 
-    create_output(main_page, districts)
-
-
+    create_output(districts)
